531786285ced40b06869bce3b481ee825d71be1c
yumi-53 <yk53713@gmail.com>
2024-08-11 22:11:58 +0900

    ページネーションを追加しよう

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::all();
+        $products = Product::paginate(15);
 
         return view('products.index', compact('products'));
 
