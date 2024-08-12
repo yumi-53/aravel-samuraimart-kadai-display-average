2e89320df1121f585ebbff9366010e3e9f25b63d
yumi-53 <yk53713@gmail.com>
2024-08-13 07:56:11 +0900

    ユーザー情報を編集できるようにしよう

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function mypage()
+    {
+        $user = Auth::User();
+        
+        return view('users.mypage', compact('user'));
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        $user = Auth::User();
+        
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        $user = Auth::user();
+
+        $user->name = $request->input('name') ? $request->input('name') : $user->name;
+        $user->email = $request->input('email') ? $request->input('email') : $user->email;
+        $user->postal_code = $request->input('postal_code') ? $request->input('postal_code') : $user->postal_code;
+        $user->address = $request->input('address') ? $request->input('address') : $user->address;
+        $user->phone = $request->input('phone') ? $request->input('phone') : $user->phone;
+        $user->update();
+
+        return to_route('mypage');
+    }
+
+}
