3cee5d7e013eaa979e42cf9b6a770976e5a31efd
yumi-53 <yk53713@gmail.com>
2024-08-27 05:19:20 +0900

    決済機能を追加しよう

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -72,35 +72,17 @@
             </div>
         </div>
 
-        <form action="{{ route('carts.destroy') }}" method="post" class="d-flex justify-content-end mt-3">
-            @csrf
-            <input type="hidden" name="_method" value="DELETE">
-            <a href="{{ route('top') }}" class="btn samuraimart-favorite-button border-dark text-dark mr-3">
+        <div class="d-flex justify-content-end mt-3">
+            <a href="{{route('top')}}" class="btn samuraimart-favorite-button border-dark text-dark mr-3">
                 買い物を続ける
             </a>
             @if ($total > 0)
-                <div class="btn samuraimart-submit-button" data-bs-toggle="modal" data-bs-target="#buy-confirm-modal">購入を確定する</div>
+                <a href="{{ route('checkout.index') }}" class="btn samuraimart-submit-button">購入に進む</a>
             @else
-                <div class="btn samuraimart-submit-button disabled" data-bs-toggle="modal" data-bs-target="#buy-confirm-modal">購入を確定する</div>
+                <button class="btn samuraimart-submit-button disabled">購入に進む</button>
             @endif
             
-            <div class="modal fade" id="buy-confirm-modal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="staticBackdropLabel">購入を確定しますか？</h5>
-                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="閉じる">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn samuraimart-favorite-button border-dark text-dark" data-bs-dismiss="modal">閉じる</button>
-                            <button type="submit" class="btn samuraimart-submit-button">購入</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\WebController;
-use Gloudemans\Shoppingcart\Facades\Cart;
+use App\Http\Controllers\CheckoutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -49,5 +49,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::get('users/carts', 'index')->name('carts.index');
         Route::post('users/carts', 'store')->name('carts.store');
         Route::delete('users/carts', 'destroy')->name('carts.destroy');
+    });
+    Route::controller(CheckoutController::class)->group(function () {
+        Route::get('checkout', 'index')->name('checkout.index');
+        Route::post('checkout', 'store')->name('checkout.store');
+        Route::get('checkout/success', 'success')->name('checkout.success');
     });
 });
