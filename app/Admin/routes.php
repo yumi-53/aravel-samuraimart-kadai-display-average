03820b0d3c72021905937e71377c0386ef4021b4
yumi-53 <yk53713@gmail.com>
2024-08-26 09:38:39 +0900

    管理画面を実装しよう #5

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -5,6 +5,8 @@ use App\Admin\Controllers\CategoryController;
 use App\Admin\Controllers\ProductController;
 use App\Admin\Controllers\MajorCategoryController;
 use App\Admin\Controllers\UserController;
+use App\Admin\Controllers\ShoppingCartController;
+
 Admin::routes();
 
 Route::group([
@@ -19,4 +21,5 @@ Route::group([
     $router->resource('products', ProductController::class);
     $router->resource('major-categories', MajorCategoryController::class);
     $router->resource('users', UserController::class);
+    $router->resource('shopping-carts', ShoppingCartController::class)->only('index');
 });
--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -63,13 +63,45 @@ class CartController extends Controller
      */
     public function destroy(Request $request)
     {
-        $user_shoppingcarts = DB::table('shoppingcart')->where('instance', Auth::user()->id)->get();
+        $user_shoppingcarts = DB::table('shoppingcart')->get();
+        $number = DB::table('shoppingcart')->where('instance', Auth::user()->id)->count();
+
         $count = $user_shoppingcarts->count();
 
         $count += 1;
+        $number += 1;
+        $cart = Cart::instance(Auth::user()->id)->content();
+
+        $price_total = 0;
+        $qty_total = 0;
+        $has_carriage_cost = false;
+
+        foreach ($cart as $c) {
+            $price_total += $c->qty * $c->price;
+            $qty_total += $c->qty;
+            if ($c->options->carriage) {
+                $has_carriage_cost = true;
+            }
+        }
+
+        if($has_carriage_cost) {
+            $price_total += env('CARRIAGE');
+        }
+
         Cart::instance(Auth::user()->id)->store($count);
 
-        DB::table('shoppingcart')->where('instance', Auth::user()->id)->where('number', null)->update(['number' => $count, 'buy_flag' => true]);
+        DB::table('shoppingcart')->where('instance', Auth::user()->id)
+            ->where('number', null)
+            ->update(
+                [
+                    'code' => substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, 10),
+                    'number' => $number,
+                    'price_total' => $price_total,
+                    'qty' => $qty_total,
+                    'buy_flag' => true,
+                    'updated_at' => date("Y/m/d H:i:s")
+                ]
+            );
 
         Cart::instance(Auth::user()->id)->destroy();
 
